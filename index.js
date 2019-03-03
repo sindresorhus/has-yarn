389841aa2c924f491a3bffde045de0a9dd67cccb
Osama Najjar <osama.najjar@mendix.com>
2019-03-03 22:04:12 +0100

    added a check if yarn is installed on the machine or not

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 'use strict';
-const path = require('path');
-const fs = require('fs');
+const {isYarnInstalled, isValidOptions, isYarnUsed} = require('./lib/lib');
 
-module.exports = cwd => fs.existsSync(path.resolve(cwd || process.cwd(), 'yarn.lock'));
+function hasYarn(...args) {
+	if (args.length > 1) {
+		throw new Error(`Invalid parameter: ${args[1]}`);
+	}
+
+	if (['string', 'object', 'undefined'].lastIndexOf(typeof args[0]) === -1) {
+		throw new Error(`Invalid parameter type.\nType ${typeof args[0]} is not supported.`);
+	}
+
+	if (typeof args[0] === 'string' || typeof args[0] === 'undefined') {
+		const cwd = args[0];
+		return isYarnUsed(cwd);
+	}
+
+	if (typeof args[0] === 'object') {
+		const options = args[0];
+		isValidOptions(options);
+		if (options.onMachine) {
+			return isYarnInstalled();
+		}
+
+		return hasYarn();
+	}
+}
+
+module.exports = hasYarn;
