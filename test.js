bc4410a922f1c495db9dc8eed1f319f065da0b29
Osama Najjar <osama.najjar@mendix.com>
2019-03-03 22:05:11 +0100

    added tests has yarn on machine

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 import test from 'ava';
+import {isYarnInstalled} from './lib/lib';
 import hasYarn from '.';
 
 test('main', t => {
 	t.true(hasYarn('fixtures/bar'));
 	t.false(hasYarn('fixtures/foo'));
 	t.false(hasYarn());
+	const invlaidParamType = 123456789;
+	const invalidParamtypeError = t.throws(() => hasYarn(invlaidParamType));
+	t.is(invalidParamtypeError.message, `Invalid parameter type.\nType ${typeof invlaidParamType} is not supported.`);
+	const invalidParamsError = t.throws(() => hasYarn('param1', 'param2', 'param3'));
+	t.is(invalidParamsError.message, 'Invalid parameter: param2');
+	const invalidOptionsError = t.throws(() => hasYarn({key1: 'val-1', key2: 'val-2', key3: 'val-3'}));
+	t.is(invalidOptionsError.message, 'Invalid options object');
+	t.is(hasYarn({onMachine: true}), isYarnInstalled());
 });
