08e968c7b075873af94d2831bc7e229478eba53b
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-31 22:55:17 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const fs = require('fs');
 const hasYarn = (cwd = process.cwd()) => fs.existsSync(path.resolve(cwd, 'yarn.lock'));
 
 module.exports = hasYarn;
+// TODO: Remove this for the next major release
 module.exports.default = hasYarn;
