ab0b0a52c51868f8fdac352b5d97d30e297b2fb7
seognil LC <seognil@gmail.com>
2019-05-31 11:35:17 +0800

    refactor: rename ./fixtures/proj-empty -> ./fixtures/proj-package_json

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 import test from 'ava';
+import {sync as commandExistsSync} from 'command-exists';
 import hasYarn from '.';
 
+const hasYarnCommand = commandExistsSync('yarn')
+
 test('main', t => {
-	t.true(hasYarn('./fixtures/proj-empty'));
+	t.is(hasYarn('./fixtures/proj-package_json'), hasYarnCommand);
 	t.true(hasYarn('./fixtures/proj-yarn-lock'));
 	t.false(hasYarn('./fixtures/proj-npm-lock'));
 	t.false(hasYarn('./fixtures/proj-node_modules'));
