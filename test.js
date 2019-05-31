344b973d5da2b7e60ee58c5f97b2499995518974
seognil LC <seognil@gmail.com>
2019-05-31 11:40:08 +0800

    test: add prue empty project (no package.json or other npm staff) test

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ import test from 'ava';
 import {sync as commandExistsSync} from 'command-exists';
 import hasYarn from '.';
 
-const hasYarnCommand = commandExistsSync('yarn')
+const hasYarnCommand = commandExistsSync('yarn');
 
 test('main', t => {
+	t.is(hasYarn('./fixtures/proj-empty'), hasYarnCommand);
 	t.is(hasYarn('./fixtures/proj-package_json'), hasYarnCommand);
 	t.true(hasYarn('./fixtures/proj-yarn-lock'));
 	t.false(hasYarn('./fixtures/proj-npm-lock'));
